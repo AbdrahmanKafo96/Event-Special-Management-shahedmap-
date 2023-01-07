3b05b01a065fd12373549996b05d94542f8e7836
Abdulrahman Kafu <73119428+AbdrahmanKafo96@users.noreply.github.com>
2023-01-07 14:41:50 +0200

    add IOS conf

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,26 +3,22 @@ import Flutter
 import GoogleMaps
 import flutter_local_notifications
 import workmanager
+import FirebaseCore
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-override func application(
-_ application: UIApplication,
-didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-) -> Bool {
-// This is required to make any communication available in the action isolate.
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-        GeneratedPluginRegistrant.register(with: registry)
-    }
-
-if #available(iOS 10.0, *) {
-  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-}
-GMSServices.provideAPIKey("AIzaSyCxMAiyFG-l2DUifjrksWErZFk_gZ8mTEk")
-UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
-GeneratedPluginRegistrant.register(with: self)
-WorkmanagerPlugin.registerTask(withIdentifier: "fetchBackground")
-
-return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-}
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
+      GMSServices.provideAPIKey("AIzaSyCxMAiyFG-l2DUifjrksWErZFk_gZ8mTEk")
+      UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+       WorkmanagerPlugin.registerTask(withIdentifier: "fetchBackground")
+      FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
